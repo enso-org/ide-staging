f17e6bb009d6ac07cdceb2f60e3284c049d500e8
Dmitry Bushev <bushevdv@gmail.com>
2021-10-13 14:23:05 +0300

    debug: impl hook

--- src/js/lib/client/tasks/prepareToSign.js
+++ src/js/lib/client/tasks/prepareToSign.js
@@ -1,6 +1,19 @@
-const fs     = require('fs');
+const fs       = require('fs');
+const path     = require('path')
+const { dist } = require('../../../../../build/paths')
 
 
+
+// =================
+// === Constants ===
+// =================
+
+const GRAALVM = 'graalvm-ce-java11-21.1.0'
+const resourcesRoot = path.join(dist.root, 'client', 'mac', 'Enso.app', 'Contents', 'Resources')
+
+const readonly = [
+    `enso/runtime/${GRAALVM}/Contents/Home/lib/server/classes.jsa`,
+]
 
 // ================
 // === Callback ===
@@ -8,4 +21,8 @@ const fs     = require('fs');
 
 exports.default = async function (context) {
     console.log("prepareToSign", { context })
+    for (let file of readonly) {
+        const target = path.join(resourcesRoot, file)
+        fs.chmodSync(target, 0o644)
+    }
 }
